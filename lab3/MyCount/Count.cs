5e8c7a0400e25f8f3997a2a3404b0d4aba853779
fl0ck1e <95759373+fl0ck1e@users.noreply.github.com>
2021-12-23 13:50:18 +0300

    Add files via upload

---
+++ lab3/MyCount/Count.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace MyCount
+{
+    public class Count
+    {
+        public int values(double s, double S, double K, double R1, double R2, double R3)
+        {
+
+            if (s > 0 & S > 0 & K > 0.47 & R1 > 0 & R2 > 0 & R3 > 0)
+            {
+                double R0 = Math.Sqrt(s / Math.PI);
+                double R = Math.Sqrt(S / Math.PI);
+                double L = K * Math.Pow (R0 / R, 2);
+                double D1 = 2 * R1;
+                double D2 = 2 * R2;
+                double D3 = 2 * R3;
+                double C = Math.Pow(D1, 2.5) + Math.Pow(D2, 1.5) + Math.Pow(D3, 0.5);
+                int N = Convert.ToInt32(L / C);
+                Console.WriteLine("Количество станций:");
+                Console.WriteLine(N);
+                return N;
+             }
+            return 8;
+        }
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MyCount;
+
+namespace START
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите площадь района обслуживания: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите площадь покрытия базовой станции");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите коэффицент застройки:");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите первый радиус: ");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второй радиус: ");
+            double e = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третий радиус: ");
+            double f = Convert.ToDouble(Console.ReadLine());
+            if (c == 0)
+            {
+                Console.WriteLine("Ошибка! Коэффицент застройки должен быть больше 0");
+            }
+            Count start = new Count();
+            int Result = start.values(a,b,c,d,e,f);
+        }
+    }
+    
+}
